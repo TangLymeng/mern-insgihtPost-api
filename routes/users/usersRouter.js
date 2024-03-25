f0879fdb3798ea3fcc37e356c11f1b56fab3ca6b
__tanglymeng <111646589+TangLymeng@users.noreply.github.com>
2024-03-25 20:40:04 +0700

    add following and unnfollwing controller

--- routes/users/usersRouter.js
+++ routes/users/usersRouter.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { register, login, getProfile, blockUser, unblockUser, profileViewers } = require("../../controllers/users/usersCtrl");
+const { register, login, getProfile, blockUser, unblockUser, profileViewers, followingUser, unFollowingUser } = require("../../controllers/users/usersCtrl");
 const isLoggin = require("../../middlewares/isLoggin");
 const usersRouter = express.Router();
 
@@ -20,6 +20,12 @@ usersRouter.put("/unblock/:userIdToUnBlock", isLoggin, unblockUser);
 
 // profile viewers
 usersRouter.get("/profile-viewer/:userProfileId", isLoggin, profileViewers);
+
+// following user
+usersRouter.put("/following/:userToFollowId", isLoggin, followingUser);
+
+// unfollowing user
+usersRouter.put("/unfollowing/:userToUnFollowId", isLoggin, unFollowingUser);
 
 // * Export
 module.exports = usersRouter;   
